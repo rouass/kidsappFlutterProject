6820fab4f46a033125b924e46f6025d85b51de58
rouass <rouasoussou@gmail.com>
2024-05-03 13:41:32 +0200

    StyleAlphabet

--- lib/LearnAlphabet.dart
+++ lib/LearnAlphabet.dart
@@ -2,38 +2,48 @@ import 'package:flutter/material.dart';
 import 'package:kidsapp/AlphabetList.dart';
 import 'package:kidsapp/main.dart';
 
-
-
 class LearnAlphabet extends StatelessWidget {
+  static final String routeName = "/alphabetScreen" ;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: Container(
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height,
-          child: Stack(
-            children: [
-              Container(
-                width: double.infinity,
-                height: MediaQuery.of(context).size.height * 0.75,
-                decoration: BoxDecoration(
-                  color: Colors.deepOrange,
-                  borderRadius: BorderRadius.only(
-                    bottomLeft: Radius.circular(120),
-                    bottomRight: Radius.circular(120),
-                  ),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Color(0xFF900D).withOpacity(0.5),
-                      spreadRadius: 5,
-                      blurRadius: 7,
-                      offset: Offset(0, 3),
-                    ),
-                  ],
+        body: Stack(
+          children: [
+            // Background image
+            Container(
+              width: double.infinity,
+              height: double.infinity,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage("images/bgRose.png"),
+                  fit: BoxFit.cover,
                 ),
+              ),
+            ),
+            // Title
+            Positioned(
+              top: 30, // Adjust the top position as needed
+              left: 0,
+              right: 0,
+              child: Center(
+                child: Text(
+                  'Alphabets Time',
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+            // Content
+            Positioned.fill(
+              child: Padding(
+                padding: const EdgeInsets.only(top: 150.0), // Adjust top padding to leave space for the title and additional margin
                 child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.start, // Align content at the top
+                  crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
                     GestureDetector(
                       onTap: () {
@@ -44,10 +54,10 @@ class LearnAlphabet extends StatelessWidget {
                       },
                       child: Container(
                         width: MediaQuery.of(context).size.width * 0.8,
-                        padding: EdgeInsets.fromLTRB(10, 5, 10, 20),
+                        padding: EdgeInsets.fromLTRB(10, 20, 10, 20), // Added margin top: 20
                         margin: EdgeInsets.only(bottom: 20),
                         decoration: BoxDecoration(
-                          color: Colors.white,
+                          color: Colors.white.withOpacity(0.7),
                           borderRadius: BorderRadius.circular(20),
                         ),
                         child: Column(
@@ -63,8 +73,8 @@ class LearnAlphabet extends StatelessWidget {
                             SizedBox(height: 10),
                             Image.asset(
                               "images/listenLearn.png",
-                              width: 100,
-                              height: 100,
+                              width: 120, // Adjusted width of the image
+                              height: 120, // Adjusted height of the image
                             ),
                           ],
                         ),
@@ -74,7 +84,7 @@ class LearnAlphabet extends StatelessWidget {
                       width: MediaQuery.of(context).size.width * 0.8,
                       padding: EdgeInsets.all(10),
                       decoration: BoxDecoration(
-                        color: Colors.white,
+                        color: Colors.white.withOpacity(0.7),
                         borderRadius: BorderRadius.circular(20),
                       ),
                       child: Column(
@@ -99,25 +109,8 @@ class LearnAlphabet extends StatelessWidget {
                   ],
                 ),
               ),
-              Positioned(
-                bottom: 0,
-                left: 0,
-                right: 0,
-                child: Container(
-                  height: MediaQuery.of(context).size.height * 0.15,
-                  color: Colors.white,
-                  child: SizedBox(
-                    width: MediaQuery.of(context).size.width * 0.15,
-                    child: Image.asset(
-                      "images/abcgroup.png",
-                      width: 20,
-                      height: 20,
-                    ),
-                  ),
-                ),
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
