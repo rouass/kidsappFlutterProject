ae8ba67b32020904e4353dfde254a1ca63218ccc
sheimazb <zbedichaima@gmail.com>
2024-05-16 13:25:38 +0100

    fix(cach)

---
+++ lib/cache/cacheHelper.dart
@@ -0,0 +1,7 @@
+//shared preferences store data key : value
+//save - get - delete - clear 
+import 'package:shared_preferences/shared_preferences.dart';
+class CachData
+{
+late SharedPreferences sharedPreferences;
+}
